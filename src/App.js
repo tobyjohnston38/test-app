dce02c28242c1e3c410c85ae7f8fcc0cc4a0304d
Toby Johnston <tobyjohnston38@gmail.com>
2018-07-23 23:16:19 +1200

    cat image yay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import cat from './cat.svg';
 import './App.css';
 import Dropdown from './Dropdown'
 import Dropup from './Dropup'
@@ -15,18 +16,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Homepage goes here</h1>
         </header>
-        <p className="App-intro">
-          Thing
-        </p>
+      <div className="App-cat">
+        <img src={cat} className="App-cat" alt="cat" />
+      </div>
         <Drops />
         <Dropdown />
         <Dropup />
         <Dropleft />
         <Dropright />
-        <footer className="App-footer">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="footer-message">Fooooooter</h1>
-        </footer>
+      <footer className="App-footer">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="footer-message">Fooooooter</h1>
+      </footer>
       </div>
     );
   }
