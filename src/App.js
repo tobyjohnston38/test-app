82f778df55053a7273e1e0ff796c13dd409752b8
Toby Johnston <toby@crssoftware.co.nz>
2018-07-31 15:18:54 +1200

    basic styled header actions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import About from './About'
 import Home from './Home'
+import Drops from './Drops'
 import Posts from './Posts'
 
 class App extends Component {
@@ -12,9 +13,18 @@ class App extends Component {
         <Router>
         <div className="App">
           <header className="App-header">
+        <div className="Home-header">
           <Link to="/">Home</Link>
+        </div>
+        <div className="About-header">
           <Link to="/about">About</Link>
+        </div>
+        <div className="Drops-header">
+          <Link to="/drops">Drops</Link>
+        </div>
+        <div className="Posts-header">
           <Link to="/posts">Posts</Link>
+        </div>
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Homepage goes here</h1>
           </header>
