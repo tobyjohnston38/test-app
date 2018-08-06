60174fa7b4cbfe6d60b62aa92284182e2b7c2f1e
Toby Johnston <tobyjohnston38@gmail.com>
2018-08-06 21:46:48 +1200

    still kinda broken

--- src/Drops.js
+++ src/Drops.js
@@ -4,29 +4,53 @@ import './App.css';
 class Drops extends Component {
   constructor() {
     super();
-    this.state ={
-      open: true
+    this.state = {
+      open: false
     }
   }
-  toggle = () => {
+  toggleUp = () => {
     this.setState({
       open: !this.state.open
     })
   }
+  toggleDown = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+  toggleLeft = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+  toggleRight = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
   render() {
     return (
-      <div onClick ={this.toggle} >
-        <div className="dropdown-container">Drop-down</div>
-          {this.state.open && <div className="dropdown-panel"/>}
-        <div className="dropup-container">Drop-up</div>
-          {this.state.open && <div className="dropup-panel"/>}
-        <div className="dropleft-container">Drop-left</div>
-          {this.state.open && <div className="dropleft-panel"/>}
-        <div className="dropright-container">Drop-right</div>
-          {this.state.open && <div className="dropright-panel"/>}
+      <div>
+        <div onClick ={this.toggleUp} >
+          <div className="dropdown-container">Drop-down</div>
+            {this.state.open && <div className="dropdown-panel"/>}
+        </div>
+        <div onClick ={this.toggleDown} >
+          <div className="dropup-container">Drop-up</div>
+            {this.state.open && <div className="dropup-panel"/>}
+        </div>
+        <div onClick ={this.toggleLeft} >
+          <div className="dropleft-container">Drop-left</div>
+            {this.state.open && <div className="dropleft-panel"/>}
+        </div>
+        <div onClick ={this.toggleRight} >
+          <div className="dropright-container">Drop-right</div>
+            {this.state.open && <div className="dropright-panel"/>}
+        </div>
       </div>
     );
-  }
+  };
 }
 
 export default Drops;
