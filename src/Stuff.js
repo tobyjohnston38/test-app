eacdeb60cd84a7222afd22f3c9d5e6a47de75a01
Toby Johnston <tobyjohnston38@gmail.com>
2018-08-07 22:03:39 +1200

    stuff page and styling

--- src/Stuff.js
+++ src/Stuff.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import Counter from './Counter'
+import Counter from "./Counter";
 
 class Stuff extends Component {
   render() {
     return (
       <Router>
-      <div>
-        <h1 className="stuff-page">About Page</h1>
-        <p className="stuff-content">Links</p>
-        <ul>
-          <li>
-            <Link to="/Counter">Counter thing</Link>
-          </li>
-        </ul>
-      </div>
-      <Route exact path="/counter" component={Counter} />
+        <div className="stuff-page">
+        <Route exact path="/counter" component={Counter} />
+          <ul>
+            <li className="Counter-thing">
+              <Link to="/Counter">Counter thing</Link>
+            </li>
+          </ul>
+        </div>
       </Router>
     );
   }
