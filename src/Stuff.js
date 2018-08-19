5875316d63e9ba745a8a385eafd0524fd5240f12
Toby Johnston <tobyjohnston38@gmail.com>
2018-08-19 14:16:46 +1200

    added route for tictac

--- src/Stuff.js
+++ src/Stuff.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import Counter from "./Counter";
 import Cards from "./Cards";
+import Tictac from "./Tictac";
 
 class Stuff extends Component {
   render() {
@@ -11,12 +12,16 @@ class Stuff extends Component {
         <div className="stuff-page">
         <Route exact path="/counter" component={Counter} />
         <Route exact path="/cards" component={Cards} />
+        <Route exact path="/tictac" component={Tictac} />
           <ul>
             <li className="Counter-thing">
               <Link to="/Counter">Counter thing</Link>
             </li>
             <li className="Cards">
               <Link to="/Cards">Cards</Link>
+            </li>
+            <li className="Tic-tac">
+              <Link to="/Tictac">Tictac</Link>
             </li>
           </ul>
         </div>
